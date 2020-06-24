22b30a32d2de59f7766317b946cdc67e2ecb0aca
MadameGeorge <magda.powalisz@gmail.com>
2020-06-24 20:58:52 +0100

    feat: Updated updateUI finction with different results, fetch Geonames data and calculate time remaining to the trip

--- src/client/js/app.js
+++ src/client/js/app.js
@@ -1,43 +1,56 @@
-/* Global Variables */
-const baseUrl = 'http://api.openweathermap.org/data/2.5/weather?zip=';
-
-// Create a new date instance dynamically with JS
-let today = new Date();
-let dayToday = today.getDate() + '.' + (today.getMonth()+1) + '.' + today.getFullYear();
-
-// Personal API Key for OpenWeatherMap API
-const apiKey = process.env.MY_API_KEY;
-
 // Event listener to add function to existing HTML DOM element
-document.getElementById('generate').addEventListener('click', updateJournal);
+document.getElementById('generate').addEventListener('click', planTrip);
 
 /* Function called by event listener */
-function updateJournal(e) {
-	// Get value of a zipcode and feelings
-	let zipCode = document.getElementById('zip').value;
-	let feelings = document.getElementById('feelings').value;
-	let queryUrl = baseUrl + zipCode + ',us' + apiKey;
-	// Run funtion
-	getApiWeather(queryUrl)
-	// Then post the data
-		.then(function(apiWeather) {
-			postJournalEntry('/add', {
-				date: dayToday, 
-				temperature: apiWeather.main.temp, 
-				content: feelings
+export function planTrip(e) {
+	
+
+	// let dayToday = today.getDate() + '.' + (today.getMonth()+1) + '.' + today.getFullYear();
+
+	// Personal API Key for OpenWeatherMap API
+	const apiKey = process.env.API_ID;
+
+	// Get value of city and date inputs
+	let cityInput = encodeURIComponent(document.getElementById('city').value);
+	let departureInput = document.getElementById('departure').value;
+	// Geonames API url
+	const baseUrl = 'http://api.geonames.org/searchJSON?q=';
+	// Create Geonames query URL with user input
+	let queryUrl = `${baseUrl} ${cityInput} &username= ${apiKey}`;
+	
+	// Call Geonames API
+	getApiGeonames(queryUrl)
+		// Then post the data to the express server
+		.then(function(apiGeonames) {
+			// Calculate days remain to the trip
+			var departureDate = new Date(departureInput);
+			let today = new Date();
+			let timeToTrip = departureDate.getTime() - today.getTime();
+			let daysToTrip = (timeToTrip / (1000 * 3600 * 24)).toFixed(0);
+			console.log(daysToTrip);
+			// Post the data as an object
+			postData('/add', {
+				departureDate: departureInput,
+				daysRemain: daysToTrip,
+				city: apiGeonames.geonames[0].name, 
+				latitude: apiGeonames.geonames[0].lat, 
+				longitude: apiGeonames.geonames[0].lng,
+				country: apiGeonames.geonames[0].countryName
 			});
 		})
+		// Then update UI
 		.then( () => {
 			updateUi();
 		});
 }
 
 /* Function to GET Web API Data*/
-const getApiWeather = async (url) => {
+const getApiGeonames = async (url) => {
 	let request = await fetch(url);
 	try {
-		const apiWeather = await request.json();
-		return apiWeather;
+		const apiGeonames = await request.json();
+		console.log(apiGeonames);
+		return apiGeonames;
 	}
 	catch(error) {
 		console.log('Something went wrong with an API: ', error);
@@ -45,17 +58,18 @@ const getApiWeather = async (url) => {
 };
 
 /* Function to POST data */
-const postJournalEntry = async ( url = '', journal = {} ) => {
+const postData = async ( url = '', data = {} ) => {
 	let response = await fetch(url, {
 		method: 'POST',
 		headers: {
 			'Content-Type': 'application/json',
 		},
-		body: JSON.stringify(journal)
+		body: JSON.stringify(data)
 	});
 	try {
-		const newJournalEntry = await response.json();
-		return newJournalEntry;
+		const newData = await response.json();
+		console.log(newData);
+		return newData;
 	}
 	catch(error) {
 		console.log('Something went wrong with fetching the post data: ', error);
@@ -66,12 +80,16 @@ const postJournalEntry = async ( url = '', journal = {} ) => {
 const updateUi = async () => {
 	const request = await fetch('/get');
 	try {
-		const journalEntry = await request.json();
-		document.getElementById('date').innerHTML = `Date: ${journalEntry.date}`;
-		document.getElementById('temp').innerHTML = `Temperature: ${journalEntry.temperature} °F`;
-		document.getElementById('content').innerHTML = journalEntry.content;
+		const tripDetails = await request.json();
+		console.log(tripDetails);
+		const days = (tripDetails.daysRemain == 1) ? 'day' : 'days';
+		console.log(days);
+		document.getElementById('city-name').innerHTML = `City: ${tripDetails.city}`;
+		document.getElementById('country-name').innerHTML = `Country: ${tripDetails.country}`;
+		document.getElementById('departure-date').innerHTML = `Country: ${tripDetails.departureDate}`;
+		document.getElementById('days-remain').innerHTML = `Your trip is in: ${tripDetails.daysRemain} ${days}`;
 	}
 	catch(error) {
 		console.log('Something went wrong with updating the UI: ', error);
 	}
-};
+}; 
